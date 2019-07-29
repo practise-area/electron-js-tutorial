8b52974741f5bf13f5f80b0562d57a9515c0dd3e
Ian Kahwai <iankahwai@gmail.com>
2019-07-29 06:59:36 +0200

    Update UI

---
+++ app/main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({ show: false });
+    mainWindow.loadFile(__dirname+'/index.html');
+
+    mainWindow.once('ready-to-show', ()=>{
+        mainWindow.show();
+    });
+    
+    mainWindow.on('closed', ()=>{
+        mainWindow = null;
+    });
+})
