7a585b197244bafa4d86297b8b552c70c9da61ca
Ian Kahwai <iankahwai@gmail.com>
2019-08-14 06:35:46 +0200

    Listing 6.19

--- app/renderer.js
+++ app/renderer.js
@@ -84,3 +84,18 @@ const getDroppedFile = (event) => event.dataTransfer.files[0];
 const fileTypeIsSupported = (file) => {
     return ['text/plain', 'text/markdown'].includes(file.type);
 };
+
+markdownView.addEventListener('dragover', (event) => {
+    const file = getDraggedFile(event);
+
+    if(fileTypeIsSupported(file)){
+        markdownView.classList.add('drag-over');
+    }else{
+        markdownView.classList.add('drag-error');
+    }
+});
+
+markdownView.addEventListener('dragleave', (event) => {
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
+})
