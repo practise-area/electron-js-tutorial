9fd07d68780d23d8a9d2c34a59c030aab9f00e4f
Ian Kahwai <iankahwai@gmail.com>
2019-08-16 06:26:56 +0200

    Listing 7.4

--- app/application-menu.js
+++ app/application-menu.js
@@ -7,6 +7,21 @@ const template = [{
     submenu: [
         //menu items given keyboard shortcuts called accelerators
         {
+            label: 'Undo',
+            accelerator: 'CommandOrControl+Z',
+            role:'undo'
+        },{
+            label: 'Redo',
+            accelerator: 'Shift+CommandOrControl+Z',
+            role:'redo'
+        },
+        {type: 'separator'},
+        {
+            label: 'Cut',
+            accelerator: 'CommandOrControl+X',
+            role:'cut'
+        },
+        {
             label: 'Copy',
             accelerator: 'CommandOrControl+C',
             role: 'copy'
@@ -14,12 +29,30 @@ const template = [{
             label: 'Paste',
             accelerator: 'CommandOrControl+V',
             role: 'paste'
-        }
+        },{
+            label: 'select All',
+            accelerator: 'CommandOrControl+Z',
+            role:'selectall'
+        },
+    ]
+}, {
+    label: 'window', 
+    subment: [
+        {
+            label: 'Minimize',
+            accelerator: 'CommandOrControl+M',
+            role:'selectall'
+        },
+            {
+                label: 'Close',
+                accelerator: 'CommandOrControl+W',
+                role:'close'
+            }
     ]
 }];
 
 if(process.platform === 'darwin'){
-    const name = 'Fire Wall';
+    const name = app.getName();
     template.unshift({
         label: none
     })
