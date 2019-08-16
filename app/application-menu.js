2869ea772a9a8652162bc81f401cb002044376b1
Ian Kahwai <iankahwai@gmail.com>
2019-08-16 06:40:52 +0200

    Listing 7.6

--- app/application-menu.js
+++ app/application-menu.js
@@ -54,7 +54,39 @@ const template = [{
 if(process.platform === 'darwin'){
     const name = app.getName();
     template.unshift({
-        label: none
+        label: none, 
+        submenu: [
+            { 
+                label: `About ${name}`,
+                role: 'about',
+            },
+            { type: 'separator'},
+            {
+                label: 'Services',
+                role: 'services',
+                submenu: [],
+            }, {
+                type: 'separator'
+            },{
+                label: `Hide ${name}`,
+                accelerator: 'Command+H',
+                role: 'hide',
+            },{
+                label: 'Hide Others',
+                accelerator: 'Command-Alt+H',
+                role: 'hideothers',
+            }, {
+                label: 'Show All',
+                role: 'unhide',
+            }, {
+                type: 'separator',
+            }, {
+                label: `Quit ${name}`,
+                accelerator: 'Command+Q',
+                click(){ app.quit(); } //no built in role for quitting application
+            }
+        ]
+            
     })
 }
 
