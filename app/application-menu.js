0ca2b0774e20a6c666ba8304eabacd668d2ae01f
Ian Kahwai <iankahwai@gmail.com>
2019-08-16 06:06:07 +0200

    Listing 7.1

---
+++ app/application-menu.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow, Menu, shell } = require('electron');
+const mainProcess = require('./main');
+
+//blueprint for the menu
+const template = [{
+    label: 'Edit',
+    submenu: [
+        //menu items given keyboard shortcuts called accelerators
+        {
+            label: 'Copy',
+            accelerator: 'CommandOrControl+C',
+            role: 'copy'
+        },{
+            label: 'Paste',
+            accelerator: 'CommandOrControl+V',
+            role: 'paste'
+        }
+    ]
+}];
+
+module.exports = Menu.buildFromTemplate(template);
