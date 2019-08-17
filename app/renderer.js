b7cf8ee0d81ac856921345a58f73a1d90fb6e692
Ian Kahwai <iankahwai@gmail.com>
2019-08-17 21:40:01 +0200

    fix bug: read null

--- app/renderer.js
+++ app/renderer.js
@@ -14,7 +14,7 @@ const newFileButton = document.querySelector('#new-file');
 const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('save-html');
+const saveHtmlButton = document.querySelector('#save-html');
 const showFileButton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
